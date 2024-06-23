fbd5ed9d169e05b80a83961048a809f673b7bd76
Harmeet <harmeet@Harmeets-MacBook-Pro.local>
2024-06-23 22:07:04 +0530

    test webhook event

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -19,7 +19,7 @@ exports.createSubscription = async (req, res) => {
           }
         );
         console.log("ad", response.data);
-        res.status(201).send({message: "Subscription created successfully"});
+        res.status(200).send({message: "Subscription created successfully"});
       } catch (error) {
         res.status(500).send({message: "Internal Server Error"});
       }
